285de12cf952da87cf032383722123d99889f2c8
Mark Lambert <marklam@spunow.co.uk>
2021-03-08 11:24:06 +0000

    Repro case

---
+++ ClassLibrary1/Library.fs
@@ -0,0 +1,88 @@
+﻿namespace ClassLibrary1
+
+open FSharp.UMX
+
+type SizeBase<'T when 'T : unmanaged> (width : int, height : int) =
+    member __.Width  with get() = width
+    member __.Height with get() = height
+
+type Size16 (width : int, height : int) =
+    inherit SizeBase<uint16>(width, height)
+
+type Size8 (width : int, height : int) =
+    inherit SizeBase<byte>(width, height)
+
+type SizeFP (width : int, height : int) =
+    inherit SizeBase<double>(width, height)
+
+type ISize<'T> =
+    abstract Width  : int with get
+    abstract Height : int with get
+
+[<Struct>]
+type SizeBaseAdapter<'pix when 'pix : unmanaged>(size : SizeBase<'pix>) =
+    interface ISize<'pix> with
+        member __.Width  with get() = size.Width
+        member __.Height with get() = size.Height
+
+type ImageBase<
+                [<Measure>]'xy,
+                'pix,
+                'pixSpace,
+                'image,
+                'createdImage
+                    when
+                        'pix          :  unmanaged      and
+                        'image        :> ISize<'pix> and
+                        'createdImage :> ISize<'pix>
+                >
+    (
+        width  : int<'xy>,
+        height : int<'xy>,
+        makeImage    : int * int     -> 'createdImage,
+        image : 'createdImage
+    ) =
+
+    new (width, height, makeImage : int * int -> 'createdImage) =
+        let dest = makeImage(int width, int height)
+        ImageBase(width, height, makeImage, dest)
+
+type Image16<[<Measure>]'xy> = ImageBase<'xy, uint16, uint16, SizeBaseAdapter<uint16>, SizeBaseAdapter<uint16>>
+
+module SizeAdapter =
+    let inline rw img = SizeBaseAdapter(img)
+
+type SizeAdapter =
+    static member inline Create(_ : SizeAdapter, _ : uint16, width, height) =
+        Size16(width, height)
+        |> SizeAdapter.rw
+
+    static member inline Create(_ : SizeAdapter, _ : byte, width, height) =
+        Size8(width, height)
+        |> SizeAdapter.rw
+
+    static member inline Create(_ : SizeAdapter, _ : float, width, height) =
+        SizeFP(width, height)
+        |> SizeAdapter.rw
+
+    static member inline create (width, height) : SizeBaseAdapter<'pix> =
+        let inline call_2 (a : ^a, b: ^b, c : int, d : int) = ((^a or ^b) : (static member Create : _*_*int*int -> _) (a, b, c, d))
+        let inline call (a : 'a, b : 'b) = fun (width, height) -> call_2 (a, b, width, height)
+        call (Unchecked.defaultof<SizeAdapter>, Unchecked.defaultof<'pix>) (width, height)
+
+[<RequireQualifiedAccess>]
+module ImageData =
+    let read skip =
+        if skip
+        then Choice1Of2 (Size8(1,1))
+        else Choice2Of2 (Size16(1,1))
+
+    let inline partial skipSections =
+        match read skipSections with
+        | Choice1Of2 img ->
+            Image16(UMX.tag<'xy> img.Width, UMX.tag<'xy> img.Height, SizeAdapter.create)
+        | Choice2Of2 img ->
+            Image16(UMX.tag<'xy> img.Width, UMX.tag<'xy> img.Height, SizeAdapter.create)
+
+    let inline load () =
+        partial true
---
+++ ConsoleApp3/Program.fs
@@ -0,0 +1,29 @@
+﻿open System.IO
+open System.Reflection.Metadata
+open System.Reflection.PortableExecutable
+
+[<EntryPoint>]
+let main _argv =
+    // Reference the code that results in the 'unknown' record
+    ClassLibrary1.ImageData.load ()
+    |> ignore
+
+    // Dump our Document records from the pdb
+    let modulePath = System.Reflection.Assembly.GetExecutingAssembly().Location
+    use moduleStream = File.OpenRead(modulePath)
+    use peReader = new PEReader(moduleStream)
+    for entry in peReader.ReadDebugDirectory() do
+        if entry.Type = DebugDirectoryEntryType.CodeView then
+            let codeViewData = peReader.ReadCodeViewDebugDirectoryData(entry)
+            if File.Exists codeViewData.Path then
+                use pdbStream = File.OpenRead(codeViewData.Path)
+                let metadataReaderProvider = MetadataReaderProvider.FromPortablePdbStream(pdbStream)
+                let metadataReader = metadataReaderProvider.GetMetadataReader()
+                for docHandle in metadataReader.Documents do
+                    let document = metadataReader.GetDocument(docHandle)
+                    if not (document.Name.IsNil) then
+                        let documentName = metadataReader.GetString(document.Name)
+                        printfn "%s" documentName
+
+    0
+
